d494cfd499c1acf9adf9989c26fd013e3effb90d
Loki Brekker <Brooliepl@gmail.com>
2024-10-03 12:18:57 +0200

    audio changes

--- data/audio.js
+++ data/audio.js
@@ -2,41 +2,41 @@ const audio = {
     map: new Howl({
         src: './audio/map.wav',
         html5: true,
-        volume: 0.1
+        volume: 1
     }),
     initBattle: new Howl({
         src: './audio/initBattle.wav',
         html5: true,
-        volume: 0.05
+        volume: 1
     }),
     battle: new Howl({
         src: './audio/battle.wav',
         html5: true,
-        volume: 0.1
+        volume: 1
     }),
     kickHit: new Howl({
         src: './audio/kickHit.wav',
         html5: true,
-        volume: 0.5
+        volume: 1
     }),
     fireballHit: new Howl({
         src: './audio/fireballHit.wav',
         html5: true,
-        volume: 0.5
+        volume: 1
     }),
     initFireball: new Howl({
         src: './audio/initFireball.wav',
         html5: true,
-        volume: 0.5
+        volume: 1
     }),
     initKick: new Howl({
         src: './audio/initKick.wav',
         html5: true,
-        volume: 0.5
+        volume: 1
     }),
     victory: new Howl({
         src: './audio/victory.wav',
         html5: true,
-        volume: 0.5
+        volume: 1
     })
 }
